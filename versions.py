1b3606dcbf0957ec01dfd74fbacb3583e7d7647a
Kenan <kenan.khauto@outlook.com>
2025-03-21 03:55:26 +0100

    Added examples to Readme

---
+++ versions.py
@@ -0,0 +1,25 @@
+import importlib
+import torch
+import clip
+import pandas as pd
+import numpy as np
+import matplotlib
+import torchvision
+import PIL
+import networkx
+import sklearn
+import seaborn as sns
+import plotly
+
+
+# Print versions
+print("Torch version:", torch.__version__)
+print("Torchvision version:", torchvision.__version__)
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Scikit-Learn version:", sklearn.__version__)
+print("Pillow (PIL) version:", PIL.__version__)
+print("NetworkX version:", networkx.__version__)
+print("clip version:", clip.__version__)
+print("seaborn version:", sns.__version__)
